d4dcfda04cf45ddfd3599c88f53771f16c97efa3
ERAF-2407 <efren.arnaude@gmail.com>
2025-11-06 17:49:06 -0400

    theWorldisyour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li><h2> ¡THE WORLD IS YOUR! </h2></li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
